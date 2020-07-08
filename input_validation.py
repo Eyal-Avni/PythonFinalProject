9e85169fe22de4f92359e40978542d2e266b1651
avshak <67116565+avshak@users.noreply.github.com>
2020-07-08 17:34:16 +0300

    Update input_validation.py

--- input_validation.py
+++ input_validation.py
@@ -1,19 +1,20 @@
 import string
 import re
 
+
 def check_email_val(string):
     length = False
     at_sign = False
     dot = False
 
-    if len(string) >=5:
+    if len(string) >= 5:
         length = True
 
     for ch in string:
-        if ch=='@':
+        if ch == '@':
             at_sign = True
 
-        if ch=='.':
+        if ch == '.':
             dot = True
 
     if length and at_sign and dot:
@@ -21,11 +22,13 @@ def check_email_val(string):
     else:
         return False
 
+
 def check_gender_input(string):
-    choice_list=['Male','Female']
+    choice_list = ['Male', 'Female']
     if string not in choice_list:
         return False
     return True
+
 
 def check_password(string):
     length = False
@@ -48,46 +51,50 @@ def check_password(string):
         print('Invalid password strength!')
         return False
 
+
 def check_ID(string):
-    if len(str(string))<9 or not string.isdigit():
+    if len(str(string)) < 9:
         return 0
     else:
         return 1
+
 
 def check_name(string):
     if string.isalpha():
         return 1
     else:
         return 0
-        
+
+
 def project_name_validation():
-    projectName=input("Enter project name\n")
-    allowedCharachters=re.compile(r'[a-zA-Z\s]*$')
+    projectName = input("Enter project name\n")
+    allowedCharachters = re.compile(r'[a-zA-Z\s]*$')
     while (not allowedCharachters.match(projectName)):
-        projectName=input("Project name is not valid, please select a project name according to instructions\n")
+        projectName = input("Project name is not valid, please select a project name according to instructions\n")
     print("Project name is valid!")
     return projectName
-    
+
+
 def project_date_validation():
-    dateFormat=re.compile(r'\d{2}[-/]\d{2}[-/]\d{4}')
-    projectDueDate=input("Enter project due date\n")
+    dateFormat = re.compile(r'\d{2}[-/]\d{2}[-/]\d{4}')
+    projectDueDate = input("Enter project due date\n")
     while (not dateFormat.match(projectDueDate)):
-        projectDueDate=input("Project Due date does not match format, please use DD/MM/YYYY\n")
-    days=projectDueDate.split("/")[0]
-    months=projectDueDate.split("/")[1]
-    years=projectDueDate.split("/")[2]
-    flags=[0,0]
+        projectDueDate = input("Project Due date does not match format, please use DD/MM/YYYY\n")
+    days = projectDueDate.split("/")[0]
+    months = projectDueDate.split("/")[1]
+    years = projectDueDate.split("/")[2]
+    flags = [0, 0]
     while (flags[0] == 0) or (flags[1] == 0):
-        if (int(days) in range(1,31)):
-            flags[0]=1
+        if (int(days) in range(1, 31)):
+            flags[0] = 1
         else:
-            days=input("days not in range, please enter a vlaue between 1 to 30\n")
-        if (int(months) in range(1,13)):
-            flags[1]=1
+            days = input("days not in range, please enter a vlaue between 1 to 30\n")
+        if (int(months) in range(1, 13)):
+            flags[1] = 1
         else:
-            months=input("months not in range, please enter a vlaue between 1 to 12\n")
-    dateList=[days,months,years]
-    projectDueDate="/".join(dateList)
+            months = input("months not in range, please enter a vlaue between 1 to 12\n")
+    dateList = [days, months, years]
+    projectDueDate = "/".join(dateList)
     return projectDueDate
 
 
